aa7d4c818ae7f5aed81857c6cb3605a62d4eb680
= <=>
2022-02-20 20:41:50 +0200

    GoodWeb3.2

--- assest/default.js
+++ assest/default.js
@@ -1,9 +1,9 @@
-const AddCurs = ()=>{
-// console.log('f');
-  const d =  document.querySelector('.course-list')
-  if(!d) return
-    d.innerHTML =  list_Our_Courses
-    .map(elem=>`
+const AddCurs = () => {
+  // console.log('f');
+  const d = document.querySelector('.course-list')
+  if (!d) return
+  d.innerHTML = list_Our_Courses
+    .map(elem => `
     <div id='courseElem' class="course-item">
     <div class="single-course mb20">
     <div class="contImgCours">
@@ -17,24 +17,43 @@ const AddCurs = ()=>{
     </div>`).join('')
 }
 
-{/* <p class="mt30" id="Frontend"><a class="enroll-button" href="#contact">Записаться на курс</a> <span class="course-price"> <span class=" course-price course-price-sale">$625</span>$425</span>
-</p> */}
+{
+  /* <p class="mt30" id="Frontend"><a class="enroll-button" href="#contact">Записаться на курс</a> <span class="course-price"> <span class=" course-price course-price-sale">$625</span>$425</span>
+  </p> */
+}
 AddCurs()
-        // <a class="item-more font20" value='7' src="./curs/${elem.page}">Подробнее</a>
-        // <a class="enroll-button" href="#contact">Записаться на курс</a>
+// <a class="item-more font20" value='7' src="./curs/${elem.page}">Подробнее</a>
+// <a class="enroll-button" href="#contact">Записаться на курс</a>
 
 
 
-        // <li><a class="1" href="#Frontend">Front-end с нуля</a></li>
-        // <li><a class="0" href="#HTML">HTML для JavaScript разработчиков</a></li>
-        // <li><a class="2" href="#JavaScript">JavaScript для Front-end</a></li>
-        // <li><a class="3" href="#React">React + Redux</a></li>
-        // <li><a class="4" href="#Python">Python</a></li>
-        // <li><a class="5" href="#Java">Java</a></li>
-        // <li><a class="6" href="#C">C#</a></li>
-        // <li><a class="7" href="#Backend">Backend</a></li>
+// <li><a class="1" href="#Frontend">Front-end с нуля</a></li>
+// <li><a class="0" href="#HTML">HTML для JavaScript разработчиков</a></li>
+// <li><a class="2" href="#JavaScript">JavaScript для Front-end</a></li>
+// <li><a class="3" href="#React">React + Redux</a></li>
+// <li><a class="4" href="#Python">Python</a></li>
+// <li><a class="5" href="#Java">Java</a></li>
+// <li><a class="6" href="#C">C#</a></li>
+// <li><a class="7" href="#Backend">Backend</a></li>
 
-        document.querySelector('.submenu').innerHTML = list_Our_Courses
-        .map((elem,ind)=>`
+document.querySelector('.submenu').innerHTML = list_Our_Courses
+  .map((elem, ind) => `
         <li><a class="${ind+1}" href="./${elem.page}">${elem.title}</a></li>
         `).join('')
+
+
+
+
+const navLi = document.querySelectorAll('#nav li')
+const arrURL = [
+  'AllCurs',
+  "index",
+  "Reviews",
+  "Question",
+  "Contact"
+]
+navLi.forEach(li => {
+  if( li.childNodes[0].href.includes(window.location.pathname)) {
+    li.classList.add('active')
+  }
+})
